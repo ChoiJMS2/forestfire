60c177ecd1b7f101b03116cd81bfc7b8b181a1c4
ChoiJMS2 <cjm881209@gmail.com>
2023-05-31 17:44:07 +0900

    update

--- eda/eda2.py
+++ eda/eda2.py
@@ -7,14 +7,12 @@ from plotly.subplots import make_subplots
 
 import streamlit as st
 from pathlib import Path
-from page import page_group
 
 def run_eda():
-    page = page_group("p")
     with st.sidebar:
-        chart1 = page.item("Chart1")
-        chart2 = page.item("Chart2")
-        chart3 = page.item("Chart3")
+        chart1 = st.checkbox("Chart1")
+        chart2 = st.checkbox("Chart2")
+        chart3 = st.checkbox("Chart3")
 
     if chart1:
         st.subheader("chart1")
@@ -24,4 +22,3 @@ def run_eda():
         st.subheader("chart3")
     else:
         st.error("Please Check to Chart")
-    page.show()

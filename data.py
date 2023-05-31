b23caed43be271a69e3884655d66f743a0247e0c
ChoiJMS2 <cjm881209@gmail.com>
2023-05-31 15:29:50 +0900

    update a; git commit -m update

--- data.py
+++ data.py
@@ -6,7 +6,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import utils
+from google.cloud import bigquery
 
+from utils import credentials, SERVICE_KEY
+client = bigquery.Client(credentials=credentials)
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(cols, name):
+    st.write("Load DataFrame")
+    sql = f"SELECT {cols} FROM erudite-river-387006.seoul.{name}"
+    df = client.query(sql).to_dataframe()
+
+    st.dataframe(df)
 def run_erd():
     pass
 

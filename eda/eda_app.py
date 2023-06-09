1bff05125d8f72231cf6eab42dc9d56faea0f2ac
ChoiJMS2 <cjm881209@gmail.com>
2023-06-09 17:30:08 +0900

    update

--- eda/eda_app.py
+++ eda/eda_app.py
@@ -9,28 +9,8 @@ from eda.map import run_map
 
 import folium
 
-@st.cache_data(ttl=600)
-def run_heatmap():
-    # 빅쿼리 클라이언트 객체 생성
-    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
-    # 쿼리 작성
-    project_id = 'forestfire-389107'
-    dataset_id = 'PreProcessing_Data'
-    table_id = 'weather_stations'
-    query = f"""
-    SELECT *
-    FROM `{project_id}.{dataset_id}.{table_id}`
-    """
-    # 쿼리 실행 및 결과를 데이터프레임으로 변환
-    combined_df = client.query(query).to_dataframe()
-    # 상관계수 행렬 계산
-    correlation_matrix = combined_df.corr(method='pearson')
-    # 히트맵 그리기
-    plt.figure(figsize=(14, 12))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", annot_kws={"size": 8})
-    plt.title('Correlation Heatmap')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.pyplot()
+def run_chart1():
+    pass
 def run_chart2():
     run_map()
 def run_chart3():
@@ -43,7 +23,7 @@ def run_eda():
     eda = st.sidebar.radio('submenu',['Chart1', 'Chart2', 'Chart3', 'Chart4'], label_visibility='collapsed')
     if eda == 'Chart1':
         st.markdown("## Chart1")
-        run_heatmap()
+        run_chart1()
     elif eda == 'Chart2':
         st.markdown("## Chart2")
         st.warning("please EDA")

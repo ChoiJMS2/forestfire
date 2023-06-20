7001f648f7fd80f7497267f87ebf047a9548ffde
ChoiJMS2 <cjm881209@gmail.com>
2023-06-20 15:53:23 +0900

    update

--- eda/eda_app.py
+++ eda/eda_app.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
-from pathlib import Path
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-from google.cloud import bigquery
-from utils import credentials
-from eda.Temp import temp_chart
 from eda.map import run_map
 from data.map_data import map_loadData
-from eda.humidity import run_humidity
 from eda.weather import weather_chart
-
-import folium
 
 def run_chart3():
     forestfire_occurs, weather_stations, gangwon_code, gangwon_UMD = map_loadData()
@@ -27,19 +17,19 @@ def run_chart3():
 
 def run_eda():
     st.sidebar.markdown("## Exploration for Data")
-    eda = st.sidebar.radio('submenu',['Temperature by region', 'Damage Map', 'Weather data by region', 'Humidity by region'], label_visibility='collapsed')
-    if eda == 'Temperature by region':
-        st.markdown("## Temperature by region")
-        temp_chart()
+    eda = st.sidebar.radio('submenu',['Weather data by region', 'Damage Map', 'Chart3', 'Chart4'], label_visibility='collapsed')
+    if eda == 'Weather data by region':
+        st.markdown("## Weather data by region")
+        weather_chart()
     elif eda == 'Damage Map':
         st.markdown("## Damage Map")
         run_map()
-    elif eda == 'Weather data by region':
-        st.markdown("## Weather data by region")
-        weather_chart()
-    elif eda == 'Humidity by region':
-        st.markdown("## Humidity by region")
-        run_humidity()
+    elif eda == 'Chart3':
+        st.markdown("## Chart3")
+        pass
+    elif eda == 'Chart4':
+        st.markdown("## Chart4")
+        pass
 
 if __name__ == '__main__':
     run_eda()
--- eda/weather.py
+++ eda/weather.py
@@ -16,7 +16,9 @@ def analysis():
                 """
 
     df = client.query(query).to_dataframe()
+    df = df[df['tm'] < '2023-01-01']
     df = df.groupby('w_regions')
+
     return df
 
 def weather_chart():
@@ -26,22 +28,19 @@ def weather_chart():
     region = ['강원중부해안', '강원중부내륙', '강원중부산지','강원북부해안', '강원북부내륙', '강원북부산지', '강원남부해안', '강원남부내륙', '강원남부산지']
     # 선택된 항목들로 데이터 그룹화
     df_groups = [group for _, group in df if _.strip() in region]
-    colors = ["pink", "lightgreen", "lightblue"]
-    box_width = 1.5  # 박스의 너비 설정
-    median_color = 'red'  # 중간 선의 색상 설정
 
-    plt.style.use('default')
-    plt.rcParams['figure.figsize'] = (24, 5)
-    plt.rcParams['font.size'] = 12
+    plt.style.use('ggplot')
+    plt.rcParams['figure.figsize'] = (15, 8)
+    plt.rcParams['font.size'] = 14
     plt.rcParams['font.family'] = 'Malgun Gothic'
     plt.rcParams['axes.unicode_minus'] = False
 
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(['기온', '습도', '강수량', '강수 유무', '풍속'])
+    tab1, tab2, tab3, tab4 = st.tabs(['기온', '습도', '강수량', '풍속'])
     with tab1:
         temp = ['평균 기온', '최저 기온', '최고 기온']
-        label1 = st.selectbox('데이터 선택', ['평균 기온', '최저 기온', '최고 기온'])
-
-        if label1 == "평균 기온":
+        selected_label = st.selectbox('데이터 선택', temp)
+        buf, col1, buf = st.columns([1, 4, 1])
+        with col1:
 
             column_mapping1 = {
                 '평균 기온': 'avgTa',
@@ -51,44 +50,181 @@ def weather_chart():
 
             fig1, ax = plt.subplots()
 
+            color_palette = ['blue', 'green', 'red']  # 항목별로 지정할 색상 리스트
+
             for i, group in enumerate(df_groups):
-                data = group['avgTa']
-                box = ax.boxplot(data, patch_artist=True, positions=[i*4+2], widths=box_width,
-                                 boxprops={'linewidth': 1.7, 'edgecolor': 'black'}, whiskerprops={'linewidth': 1.7})
+                if selected_label in column_mapping1 and column_mapping1[selected_label] in group.columns:
+                    data = [group[column_mapping1[selected_label]]]
+                    box = ax.boxplot(data, patch_artist=True, positions=[i * 2], widths=0.8,
+                                     boxprops={'linewidth': 1.7, 'edgecolor': 'black', 'facecolor': color_palette[1]},
+                                     whiskerprops={'linewidth': 1.7},
+                                     medianprops={'color': 'red'})
 
-                for patch, color in zip(box["boxes"], colors):
-                    patch.set_facecolor(color)
-
-                for median in box["medians"]:
-                    median.set(color=median_color)  # 중간 선의 색상 설정
-
-            ax.set_xticks([i * 4 + 2 for i in range(len(df_groups))])
-            ax.set_xticklabels(region)
+            xticks = np.arange(len(df_groups)) * 2
+            ax.set_xticks(xticks)  # positions 값으로 x축 설정
+            ax.set_xticklabels(region, rotation=0, ha='center')
 
             yticks = np.arange(-30, 50, 10)
             ax.set_yticks(yticks)
 
-            legend_patches = [mpatches.Patch(color=color, label=label) for color, label in zip(colors, label1)]
-            ax.legend(handles=legend_patches, loc='lower center', bbox_to_anchor=(0.5, -0.2), fontsize=8.5,
-                      ncol=len(df_groups))
-
-            ax.set_title('9개 지역별 평균 기온 데이터', fontweight='bold')
+            ax.set_title('9개 지역별 {} 데이터'.format(selected_label), fontweight='bold')
             ax.set_ylabel('온도 (℃)')
             plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
 
             st.pyplot(fig1)
 
     with tab2:
-        pass
+        humidity = ['평균 상대습도', '최저 상대습도', '실효습도']
+        selected_label2 = st.selectbox('데이터 선택', humidity)
+        buf, col2, buf = st.columns([1, 4, 1])
+        with col2:
+
+            column_mapping2 = {
+                '평균 상대습도': 'avgRhm',
+                '최저 상대습도': 'minRhm',
+                '실효습도': 'effRhm'
+                }
+
+            fig2, ax = plt.subplots()
+
+            color_palette = ['blue', 'green', 'red']  # 항목별로 지정할 색상 리스트
+
+            for i, group in enumerate(df_groups):
+                if selected_label2 in column_mapping2 and column_mapping2[selected_label2] in group.columns:
+                    data = [group[column_mapping2[selected_label2]]]
+                    box = ax.boxplot(data, patch_artist=True, positions=[i * 2], widths=0.8,
+                                     boxprops={'linewidth': 1.7, 'edgecolor': 'black', 'facecolor': color_palette[1]},
+                                     whiskerprops={'linewidth': 1.7},
+                                     medianprops={'color': 'red'})
+
+            xticks = np.arange(len(df_groups)) * 2
+            ax.set_xticks(xticks)  # positions 값으로 x축 설정
+            ax.set_xticklabels(region, rotation=0, ha='center')
+
+            yticks = np.arange(0, 110, 10)
+            ax.set_yticks(yticks)
+
+            ax.set_title('9개 지역별 {} 데이터'.format(selected_label2), fontweight='bold')
+            ax.set_ylabel('습도 (%)')
+            plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
+
+            st.pyplot(fig2)
+
 
     with tab3:
-        pass
+        rainfall = ['일일 강수량', '7일간 강수량', '비가 오지 않은 날', '강수 여부']
+        selected_label3 = st.selectbox('데이터 선택', rainfall)
+        buf, col3, buf = st.columns([1, 6, 1])
+        with col3:
+            if selected_label3 == '강수 여부':
+                plt.rcParams['figure.figsize'] = (10, 5)
+
+                fig5, ax = plt.subplots()
+
+                width = 0.35
+                x = range(len(region))
+                colors = ['steelblue', 'darkorange']
+
+                legend_labels = ['0', '1']
+
+                for i, df in enumerate(df_groups):
+                    counts = df['Rntf'].value_counts()
+
+                    ax.bar(x[i] - width / 2, counts[0], width, label=legend_labels[0], color=colors[0])
+                    ax.bar(x[i] + width / 2, counts[1], width, label=legend_labels[1], color=colors[1])
+
+                    for j, count in enumerate(counts):
+                        ax.text(x[i] + width * (j - 0.5), count + 2, str(count), ha='center', va='bottom')
+
+                ax.set_xticks(x)
+                ax.set_xticklabels(region)
+
+                ax.set_ylabel('단위 (건)')
+                ax.set_title('지역별 강수 여부')
+
+                legend = ax.legend(handles=[ax.patches[0], ax.patches[len(region)]], labels=legend_labels,
+                                   title='강수 여부',
+                                   loc='upper center', ncol=2)
+
+                legend.set_bbox_to_anchor((0.5, -0.15))
+                legend._legend_box.align = "center"
+
+                plt.tight_layout()
+                st.pyplot(fig5)
+
+            elif selected_label3 == '비가 오지 않은 날':
+                fig4, ax = plt.subplots()
+                for i, group in enumerate(df_groups):
+                    data = group["noRn"]
+                    plt.scatter([i + 1] * len(data), data, s=100, label=region)
+
+                plt.xticks(range(1, len(region) + 1), region)
+                plt.title(f'9개 지역별 {selected_label3}', fontweight='bold')
+                plt.ylabel('단위 (일)')
+
+                st.pyplot(fig4)
+            else:
+                column_mapping3 = {
+                    '일일 강수량': 'sumRn',
+                    '7일간 강수량': 'sumRn7'
+                }
+
+                fig3, ax = plt.subplots()
+
+                for i, group in enumerate(df_groups):
+                    if selected_label3 in column_mapping3 and column_mapping3[selected_label3] in group.columns:
+                        data = group[column_mapping3[selected_label3]]
+                        plt.scatter([i+1] * len(data), data, s=100, label=region)
+
+                plt.xticks(range(1, len(region)+1), region)
+                plt.title(f'9개 지역별 {selected_label3}', fontweight='bold')
+                plt.ylabel('강수량 (mm)')
+
+                st.pyplot(fig3)
 
     with tab4:
-        pass
+        wind = ['평균 풍속', '최대 풍속', '최대 순간 풍속', '7일간 최대 풍속']
+        selected_label4 = st.selectbox('데이터 선택', wind)
+        buf, col4, buf = st.columns([1, 6, 1])
+        with col4:
+            column_mapping4 = {
+                '평균 풍속': 'avgWs',
+                '최대 풍속': 'maxWs',
+                '최대 순간 풍속': 'maxInsWs',
+                '7일간 최대 풍속': 'maxwind7'
+                }
 
-    with tab5:
-        pass
+            fig6, ax = plt.subplots()
+
+            color_palette = ['blue', 'green', 'red']  # 항목별로 지정할 색상 리스트
+
+            for i, group in enumerate(df_groups):
+                if selected_label4 in column_mapping4 and column_mapping4[selected_label4] in group.columns:
+                    data = [group[column_mapping4[selected_label4]]]
+                    box = ax.boxplot(data, patch_artist=True, positions=[i * 2], widths=0.8,
+                                     boxprops={'linewidth': 1.7, 'edgecolor': 'black', 'facecolor': color_palette[1]},
+                                     whiskerprops={'linewidth': 1.7},
+                                     medianprops={'color': 'red'})
+
+            xticks = np.arange(len(df_groups)) * 2
+            ax.set_xticks(xticks)  # positions 값으로 x축 설정
+            ax.set_xticklabels(region, rotation=0, ha='center')
+
+            if selected_label4 == '평균 풍속':
+                yticks = np.arange(0, 20, 5)
+            elif selected_label4 == '최대 순간 풍속':
+                yticks = np.arange(0, 40, 5)
+            else:
+                yticks = np.arange(0, 30, 5)
+            ax.set_yticks(yticks)
+
+            ax.set_title('지역별 {}'.format(selected_label4), fontweight='bold')
+            ax.set_ylabel('습도 (%)')
+            plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
+
+            st.pyplot(fig6)
+
+
 
 if __name__ == "__main__":
     weather_chart()
